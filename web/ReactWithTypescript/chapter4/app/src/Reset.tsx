30a8f37e73fa2d93613d985995f6d7c1530babfb
Eduardo Yair Cuellar Rivera <eycr.nm@gmail.com>
2023-06-22 19:19:45 -0600

    Covered Part 1 of the book

---
+++ web/ReactWithTypescript/chapter4/app/src/Reset.tsx
@@ -0,0 +1,9 @@
+import { memo } from "react";
+type Props = {
+  onClick: () => void;
+};
+export const Reset = memo(({ onClick }: Props) => {
+  console.log("render Reset");
+  return <button onClick={onClick}>Reset</button>;
+});
+Reset.displayName = "Reset";
